f68811f0b1193390e33f14e5cc888d2b7ac1cbd6
Robert Hensing <robert@roberthensing.nl>
2019-02-25 13:34:50 +0100

    Use overlay in /module.nix

---
+++ nix/overlay.nix
@@ -0,0 +1,7 @@
+self: pkgs: {
+  # packages defined in this repo
+  hercules-ci-agent-packages = pkgs.callPackages ./packages.nix {};
+
+  # overrides
+  hercules-ci-agent = pkgs.haskell.lib.justStaticExecutables self.hercules-ci-agent-packages.hercules-ci-agent;
+}
