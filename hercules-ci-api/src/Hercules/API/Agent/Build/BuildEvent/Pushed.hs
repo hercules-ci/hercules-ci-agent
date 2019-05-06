2893168ad215fd13d261f29d913acf5579b9ed49
Robert Hensing <robert@roberthensing.nl>
2019-05-06 14:25:36 +0200

    api, agent: Emit Pushed event

---
+++ hercules-ci-api/src/Hercules/API/Agent/Build/BuildEvent/Pushed.hs
@@ -0,0 +1,8 @@
+{-# LANGUAGE DeriveAnyClass #-}
+module Hercules.API.Agent.Build.BuildEvent.Pushed where
+
+import Hercules.API.Prelude
+
+data Pushed = Pushed
+ { cache :: Text
+ } deriving (Generic, Show, Eq, ToJSON, FromJSON, ToSchema)
