d7c2ba6a9646502064c0c39b4db6abb3f8006317
Robert Hensing <robert@roberthensing.nl>
2019-05-07 14:18:43 +0200

    Remove nixos-18.09 support

--- hercules-ci-api/src/Hercules/API/Orphans.hs
+++ hercules-ci-api/src/Hercules/API/Orphans.hs
@@ -1,15 +1,3 @@
 {-# OPTIONS_GHC -fno-warn-orphans #-}
 {-# LANGUAGE CPP #-}
 module Hercules.API.Orphans where
-
-import           Servant.Auth.Swagger           ( )
-
-#if !MIN_VERSION_swagger2(2,3,1)
-import           Data.Monoid                    ( mempty )
-import           Data.Swagger
-import           Web.Cookie                     ( SetCookie )
-
--- nixpkgs <= 18.09
-instance ToParamSchema SetCookie where
-  toParamSchema _ = mempty -- TODO: cookie instances for swagger
-#endif
