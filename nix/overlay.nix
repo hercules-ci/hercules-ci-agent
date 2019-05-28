9b198d1b6eb82bd01820322542f9f7855348560e
Domen Kožar <domen@dev.si>
2019-05-28 13:12:55 +0700

    fix shell.nix

--- nix/overlay.nix
+++ nix/overlay.nix
@@ -1,8 +1,20 @@
 self: pkgs:
-{
+
+let
+  sources = import ./sources.nix;
+in {
   # packages defined in this repo
   hercules-ci-agent-packages = pkgs.callPackages ./packages.nix {};
 
   # overrides
   hercules-ci-agent = pkgs.haskell.lib.justStaticExecutables self.hercules-ci-agent-packages.hercules-ci-agent;
+
+  devTools = {
+    inherit (pkgs)
+      shellcheck
+      ;
+    inherit (import sources.niv {})
+      niv
+      ;
+  };
 }
--- shell.nix
+++ shell.nix
@@ -1,4 +1,10 @@
-with { pkgs = import ./nix {}; };
-pkgs.mkShell
-  { buildInputs = [ pkgs.devTools.niv ];
-  }
+{ sources ? import ./nix/sources.nix
+, nixpkgsSource ? "nixos-19.03"
+, nixpkgs ? sources."${nixpkgsSource}"
+}:
+
+let
+  pkgs = import nixpkgs { overlays = [ (import ./nix/overlay.nix) ] ; config = {}; };
+in pkgs.mkShell {
+  buildInputs = [ pkgs.devTools.niv pkgs.devTools.shellcheck ];
+}
