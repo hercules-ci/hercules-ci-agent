d40b20d0c6f27113a78ce96a0a4a6009fa59fce6
Robert Hensing <robert@roberthensing.nl>
2019-05-31 12:30:27 +0200

    profiles: Improve error messages

--- for-upstream-only/nixos/profiles/hercules-ci-agent.nix
+++ for-upstream-only/nixos/profiles/hercules-ci-agent.nix
@@ -5,13 +5,18 @@
 { pkgs, lib, config, ...}:
 let
   cfg = config.profile.hercules-ci-agent;
-  inherit (lib) types isAttrs mkIf escapeShellArg;
+  inherit (lib) types isAttrs mkIf mkDefault escapeShellArg;
 
-  ifCachix = mkIf (cfg.cachixSecretsFile != null);
+  ifCachix = mkIf (cfg.cachixSecretsFile != null || cfg.cachixDeployedSecretsPath != null);
+
+  # Don't set it or the cachix module will emit confusing errors. This module
+  # should fully wrap this aspect of deployment to avoid mistakes.
+  ifCachixAndOk = mkIf (cfg.cachixSecretsFile != null && cfg.cachixDeployedSecretsPath != null);
 
 in
 {
   options = {
+
     profile.hercules-ci-agent.freespaceGB = lib.mkOption {
       type = types.int;
       default = 30;
@@ -19,6 +24,24 @@ in
         Amount of free space (GB) to ensure on garbage collection
       '';
     };
+
+    # This option is replicated here in order to improve error messages.
+    profile.hercules-ci-agent.cachixDeployedSecretsPath = lib.mkOption {
+      type = types.nullOr types.str;
+      default = null;
+      description = ''
+        If not null, this option must point to a file on the deployed machine
+        by means of a literal string, in order to avoid putting secrets in the
+        Nix store.
+
+        The file must be readable by the hercules-ci-agent user. Part of the
+        file will be made available to the Nix daemon exclusively.
+
+        Sets both nix.cachix.deployedSecretsPath and
+        services.hercules-ci-agent.cachixSecretsPath.
+      '';
+    };
+
     profile.hercules-ci-agent.cachixSecretsFile = lib.mkOption {
       type = types.nullOr types.path;
       default = null;
@@ -35,18 +58,45 @@ in
         services.hercules-ci-agent.cachixSecretsPath.
       '';
     };
+
   };
 
   imports = [ ./cachix-use-export.nix ];
 
   config = {
     services.hercules-ci-agent.enable = true;
+    services.hercules-ci-agent.cachixSecretsPath = cfg.cachixDeployedSecretsPath;
 
-    nix.cachix.secretsFile = ifCachix cfg.cachixSecretsFile;
-    nix.cachix.deployedSecretsPath = ifCachix config.services.hercules-ci-agent.cachixSecretsPath;
+    nix.cachix.secretsFile = ifCachixAndOk cfg.cachixSecretsFile;
+    nix.cachix.deployedSecretsPath = ifCachixAndOk cfg.cachixDeployedSecretsPath;
 
     nix.gc.automatic = true;
     nix.gc.options = ''--max-freed "$((${toString config.profile.hercules-ci-agent.freespaceGB} * 1024**3 - 1024 * $(df -P -k /nix/store | tail -n 1 | ${pkgs.gawk}/bin/awk '{ print $4 }')))"'';
 
+    # These are essentially duplications of the cachix module, but doing so is
+    # required in order to provide the right error messages.
+    assertions = ifCachix [
+      {
+        assertion = cfg.cachixDeployedSecretsPath != null -> cfg.cachixSecretsFile != null;
+        message = ''
+          You need to specify profile.hercules-ci-agent.cachixSecretsFile in
+          order to provide the non-sensitive parts of the cache configuration.
+
+          WARNING: If you've used a path expression in
+          profile.hercules-ci-agent.cachixDeployedSecretsPath, you may want to
+          delete it from your Nix store!
+        '';
+      }
+
+      # TODO: not required when file is not sensitive.
+      {
+        assertion = cfg.cachixSecretsFile != null -> cfg.cachixDeployedSecretsPath != null;
+        message = ''
+          You need to deploy the Cachix secrets file to the machine outside the
+          Nix store and set profile.hercules-ci-agent.cachixDeployedSecretsPath to
+          the location of the deployed file.
+        '';
+      }
+    ];
   };
 }
