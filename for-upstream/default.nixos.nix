ee4fb25bf2ea43669bfef0e44852b2f2db9e72dd
Robert Hensing <robert@roberthensing.nl>
2019-09-03 18:09:23 +0200

    agent: Make auto restart on secrets deployment work
    Path units do not seem to be capable of restarting services, so
    this introduces a oneshot service that is *started* and restarts
    the actual service.

--- for-upstream/default.nixos.nix
+++ for-upstream/default.nixos.nix
@@ -40,11 +40,22 @@ in
       };
     };
 
-    systemd.paths.hercules-ci-agent = {
+    systemd.paths.hercules-ci-agent-restart-files = {
       wantedBy = [ "hercules-ci-agent.service" ];
       pathConfig = {
-        PathChanged = [ cfg.secretsDirectory ];
+        Unit = "hercules-ci-agent-restarter.service";
+        PathChanged = [
+          cfg.finalConfig.clusterJoinTokenPath
+          cfg.finalConfig.binaryCachesPath
+        ];
       };
+    };
+
+    systemd.services.hercules-ci-agent-restarter = {
+      serviceConfig.Type = "oneshot";
+      script = ''
+        systemctl restart hercules-ci-agent.service
+      '';
     };
 
     users = mkIf (cfg.user == defaultUser) {
