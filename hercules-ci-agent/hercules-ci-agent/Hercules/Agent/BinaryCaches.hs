5f8cc6826bdc1b62ef1063ef3deb1b7f183ee9d8
Robert Hensing <robert@roberthensing.nl>
2019-09-13 12:37:37 +0200

    agent: Improve binary-caches.json error reporting

--- hercules-ci-agent/hercules-ci-agent/Hercules/Agent/BinaryCaches.hs
+++ hercules-ci-agent/hercules-ci-agent/Hercules/Agent/BinaryCaches.hs
@@ -7,6 +7,7 @@ module Hercules.Agent.BinaryCaches
     )
 where
 
+import Control.Exception.Lifted (catchJust)
 import Data.Aeson
 import qualified Data.ByteString.Lazy as BL
 import qualified Data.Map as M
@@ -15,18 +16,14 @@ import Hercules.Agent.Config
 import Hercules.Agent.Log
 import Hercules.Error
 import Hercules.Formats.CachixCache
-import Protolude
-import System.Directory (doesFileExist)
-import System.FilePath ((</>))
+import Protolude hiding (catchJust)
+import System.IO.Error (isDoesNotExistError)
 
 data BinaryCaches
   = BinaryCaches
       { cachixCaches :: Map Text CachixCache,
         unknownKinds :: Map Text UnknownKind
         }
-
-noCaches :: BinaryCaches
-noCaches = BinaryCaches mempty mempty
 
 data UnknownKind = UnknownKind {kind :: Text}
   deriving (Generic, FromJSON)
@@ -44,7 +41,21 @@ instance FromJSON BinaryCaches where
 parseFile :: Config 'Final -> KatipContextT IO BinaryCaches
 parseFile cfg = do
   let fname = binaryCachesPath cfg
-  bytes <- liftIO $ BL.readFile $ toS fname
+  bytes <-
+    catchJust
+      (mfilter isDoesNotExistError . Just)
+      (liftIO (BL.readFile (toS fname)))
+      ( \_e ->
+          liftIO $ throwIO $ FatalError
+            $ "The binary-caches.json file does not exist.\
+              \\nAccording to the configuration, it should be in\
+              \\n  "
+            <> toS fname
+            <> "\
+              \\n\
+              \\nFor more information about binary-caches.json, see\n\
+              \\nhttps://docs.hercules-ci.com/hercules-ci/reference/binary-caches-json/"
+        )
   bcs <- escalateAs (FatalError . toS) $ eitherDecode bytes
   validate (toS fname) bcs
   pure bcs
@@ -56,7 +67,9 @@ validate fname bcs = do
         WarningS
         "You did not configure any caches. This is ok for evaluation purposes,\
         \ but a cache is required for multi-agent operation and\
-        \ to work well across garbage collection."
+        \ to work well across garbage collection.\
+        \ For more information about binary-caches.json, see\
+        \ https://docs.hercules-ci.com/hercules-ci/reference/binary-caches-json/"
   forM_ (M.toList (unknownKinds bcs)) $ \(k, v) ->
     logLocM WarningS
       $ "In file "
