bfd0cfcc8a04844095b8d9ffdbcaed5655d735cb
Robert Hensing <robert@roberthensing.nl>
2020-01-30 09:42:34 +0100

    nix/haskell-inline-c-cpp.nix: Work around sdist problem

--- nix/haskell-inline-c-cpp.nix
+++ nix/haskell-inline-c-cpp.nix
@@ -6,11 +6,18 @@
 , safe-exceptions
 , stdenv
 , template-haskell
+, fetchgit
 }:
 mkDerivation {
   pname = "inline-c-cpp";
   version = "0.4.0.1";
-  sha256 = "187b526453791f8c97be4daa4dc153c0b3c034df4815dcaa99f4cb8eb5a955af";
+  # Fetching from git only because of https://github.com/fpco/inline-c/pull/106
+  src = fetchgit {
+    url = "https://github.com/fpco/inline-c";
+    sha256 = "1yn5l9962imc3qq1hzz1gvs49f4j0vnb5q3yf4lc3ssdxkf4nd2n";
+    rev = "dc7f8aac5d689630b77a74d762e4e5a633322d8d";
+    fetchSubmodules = true;
+  } + "/inline-c-cpp";
   libraryHaskellDepends = [
     base
     containers
