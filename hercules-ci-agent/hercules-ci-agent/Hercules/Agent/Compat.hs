ac2f1c9dd7a85fe7febf9d8da6ebd3eb2b39f2a2
Robert Hensing <robert@roberthensing.nl>
2020-02-17 10:05:34 +0100

    Fix warnings

--- hercules-ci-agent/hercules-ci-agent/Hercules/Agent/Compat.hs
+++ hercules-ci-agent/hercules-ci-agent/Hercules/Agent/Compat.hs
@@ -2,13 +2,11 @@
 module Hercules.Agent.Compat where
 
 import qualified Katip as K
-import Protolude 
 
 #if MIN_VERSION_katip(0,8,0)
 katipLevel :: K.Severity -> K.PermitFunc
 katipLevel =
   K.permitItem
 #else
-katipLevel =
-  identity
+katipLevel x = x
 #endif
