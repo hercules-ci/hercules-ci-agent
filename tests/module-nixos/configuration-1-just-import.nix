7ef9b9bbc0af5847680cd020bb05b241260facbe
Robert Hensing <robert@roberthensing.nl>
2020-10-27 12:07:18 +0100

    Remove module tests
    They're broken by the flake's hashless eval-time fetching.
    Breaking changes are coming for the 0.8 release anyway.

--- tests/module-nixos/configuration-1-just-import.nix
+++
@@ -1,5 +0,0 @@
-{ ... }: {
-
-  imports = [ ./configuration-0.nix ../../module.nix ];
-
-}
--- tests/module-nixos/configuration-3-nixops-base-case.nix
+++
@@ -1,8 +0,0 @@
-{ ... }: {
-
-  imports = [ ./configuration-2-base-case.nix ];
-
-  deployment.keys."binary-caches.json".text = "{}";
-  deployment.keys."cluster-join-token.key".keyFile = ./cluster-join-token.key;
-
-}
