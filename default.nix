3c782ed8079337741c410fec271bd42e10b0ba61
Robert Hensing <robert@roberthensing.nl>
2020-10-30 12:25:17 +0100

    default.nix: Add backward compatibility

--- default.nix
+++ default.nix
@@ -1,1 +1,5 @@
 (import ./nix/flake-compat.nix).defaultNix
+  // {
+  # Compatibility with pre-flake default.nix
+  __functor = self: { system ? builtins.currentSystem }: self.packages.${system};
+}
