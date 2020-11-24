c8f07298542c95e960a56b8916361cd8a72b8a18
Robert Hensing <robert@roberthensing.nl>
2020-11-24 21:28:34 +0100

    Switch development to cabal
    The ghcide/haskell-language-server setup broke again with stack,
    unable to find the aeson package.

--- shell.nix
+++ shell.nix
@@ -1,24 +1,32 @@
-{ sources ? import ./nix/sources.nix
-, nixpkgsSource ? "nixos-unstable"
-, nixpkgs ? sources."${nixpkgsSource}"
+{ pkgs ? import ./nix/default.nix {}
+, ...
 }:
+let hs = pkgs.hercules-ci-agent-packages.internal.haskellPackages;
+    inherit (pkgs.hercules-ci-agent-packages.internal.pkgs) devTools;
+in hs.shellFor {
+  packages = p: [
+    p.hercules-ci-api-core
+    p.hercules-ci-api
+    p.hercules-ci-api-agent
+    p.hercules-ci-agent
+    p.hercules-ci-agent-test
+  ];
+  buildInputs = [
+    pkgs.boost
+    pkgs.nix
+  ];
+  nativeBuildInputs = [
+    pkgs.yaml2json
+    pkgs.jq
+    # hs.stack
+    (hs.cabal-install.overrideScope (self: super: { Cabal = self.Cabal_3_2_1_0; }))
+    hs.haskell-language-server
 
-let
-  pkgs =
-    import nixpkgs { overlays = [ (import ./nix/overlay.nix) ]; config = {}; };
-  agentpkgs = import ./default.nix {};
-in
-pkgs.mkShell {
-  buildInputs =
-    [
-      agentpkgs.devTools.niv
-      agentpkgs.devTools.stack
-      agentpkgs.devTools.ghcid
-      agentpkgs.devTools.jq
-      agentpkgs.devTools.cabal2nix
-      agentpkgs.devTools.nix-prefetch-git
-      agentpkgs.hercules-ci-agent-packages.internal.haskellPackages.ghc
-      agentpkgs.hercules-ci-agent-packages.internal.haskellPackages.ghcide
-    ];
-  inherit (agentpkgs.hercules-ci-agent-packages.pre-commit-check) shellHook;
+    devTools.niv
+    devTools.ghcid
+    devTools.jq
+    devTools.cabal2nix
+    devTools.nix-prefetch-git
+  ];
+  inherit (pkgs.hercules-ci-agent-packages.pre-commit-check) shellHook;
 }
