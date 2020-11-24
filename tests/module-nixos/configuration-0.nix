c7e1fd6f0baaa0d2a0b0893c7e224f2f5e8db973
Robert Hensing <robert@roberthensing.nl>
2020-11-24 21:53:38 +0100

    Remove test/module-nixos, almost obsolete

--- tests/module-nixos/configuration-0.nix
+++
@@ -1,7 +0,0 @@
-{ config, lib, ... }:
-
-{
-  # to please nixos
-  boot.loader.grub.enable = false;
-  fileSystems."/".device = "/dev/null";
-}
--- tests/module-nixos/test.nix
+++
@@ -1,8 +0,0 @@
-{ nixpkgs, modules }:
-
-# this runs in the sandbox, so the impurities of nixos glue are not a problem
-(
-  import (nixpkgs + "/nixos/lib/eval-config.nix") {
-    inherit modules;
-  }
-).config.system.build.toplevel
