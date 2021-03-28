97a930101d909eaacdf42662823bc5150fd973e1
Robert Hensing <robert@roberthensing.nl>
2021-03-28 20:45:42 +0200

    Test with multiple nix-daemon versions and use correct agent build

--- tests/agent-test.nix
+++ tests/agent-test.nix
@@ -1,3 +1,4 @@
+{ daemonIsNixUnstable }:
 { pkgs, ... }:
 let
   tarball = x: pkgs.runCommand "${x.name or "tarball"}.tar.gz"
@@ -19,9 +20,11 @@ let
 
   runnerArgs = '''';
 
+  inherit (pkgs.lib) optionalString;
+
 in
 {
-  name = "agent-test";
+  name = "agent-test${optionalString daemonIsNixUnstable "-daemon-nixUnstable"}";
 
   nodes = {
     agent = { config, pkgs, lib, ... }: {
@@ -33,7 +36,10 @@ in
         environment.etc."reference-stdenv".text = builtins.toJSON (pkgs.runCommand "foo" { } "").drvAttrs;
         # It's an offline test, so no caches are available
         nix.binaryCaches = lib.mkForce [ ];
+        nix.package = lib.mkIf daemonIsNixUnstable pkgs.nixUnstable;
         services.hercules-ci-agent.enable = true;
+        # Instead of the default, we want the nix library version from the build matrix (which should include at least the default)
+        services.hercules-ci-agent.package = lib.mkForce pkgs.hercules-ci-agent;
         services.hercules-ci-agent.settings.apiBaseUrl = "http://api";
         services.hercules-ci-agent.settings.nixUserIsTrusted = lib.mkForce false;
         services.hercules-ci-agent.settings.binaryCachesPath = (pkgs.writeText "binary-caches.json" (builtins.toJSON { })).outPath;
