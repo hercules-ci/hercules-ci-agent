f88ed682f023e458e2130fd312d35e8817bcb642
Robert Hensing <robert@roberthensing.nl>
2021-04-27 17:04:49 +0200

    hercules-api-core: Add servant-generic utilities

--- hercules-ci-api-core/src/Hercules/API/Servant.hs
+++ hercules-ci-api-core/src/Hercules/API/Servant.hs
@@ -1,5 +1,21 @@
+{-# LANGUAGE PolyKinds #-}
+{-# LANGUAGE TypeFamilies #-}
+
 -- | Extras for working with servant
-module Hercules.API.Servant where
+module Hercules.API.Servant
+  ( noContent,
+
+    -- * 'Generic'
+    useApi,
+    useApiE,
+    enterApi,
+    enterApiE,
+
+    -- * Substitution
+    Substitute,
+    Placeholder,
+  )
+where
 
 import Control.Monad (void)
 import Servant.API
@@ -18,9 +34,49 @@ useApi ::
   g mode
 useApi = (Servant.API.Generic.fromServant .)
 
+-- | Like 'useApi' but constrains the @auth@ type variable that's passed to
+-- subapis.
+useApiE ::
+  forall m api rg a.
+  (GenericServant (api a) m, GenericServant (rg a) m) =>
+  (api a m -> ToServant (rg a) m) ->
+  api a m ->
+  rg a m
+useApiE = useApi
+
+-- | @flip 'useApi'
+enterApi ::
+  forall g f mode.
+  (GenericServant f mode, GenericServant g mode) =>
+  f mode ->
+  (f mode -> ToServant g mode) ->
+  g mode
+enterApi = flip useApi
+
+-- | @flip 'useApiE'
+enterApiE ::
+  forall m api rg a.
+  (GenericServant (api a) m, GenericServant (rg a) m) =>
+  api a m ->
+  (api a m -> ToServant (rg a) m) ->
+  rg a m
+enterApiE = flip useApi
+
 -- | 'Control.Monad.void' specialised to 'NoContent' to soothe the
 -- compiler that rightfully warns about throwing away a do notation
 -- result. By specialising, we make sure that we still get warnings
 -- if the result type changes in the future. (We'll get an error)
 noContent :: Functor m => m Servant.API.NoContent -> m ()
 noContent = void
+
+-- | A reference to the @subapi@ parameter in 'Substitute'
+data Placeholder
+
+-- | Replaces 'Placeholder' by @subapi@ in the API @target@.
+type family Substitute (target :: k) subapi :: *
+
+type instance Substitute Placeholder subapi = subapi
+
+type instance Substitute (a :> b) subapi = a :> Substitute b subapi
+
+type instance Substitute (a :<|> b) subapi = Substitute a subapi :<|> Substitute b subapi
