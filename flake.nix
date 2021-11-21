6475bb35eac5b4b786bb666e885c22c055f57499
Robert Hensing <robert@roberthensing.nl>
2021-11-21 17:51:17 +0100

    Put flake outputs in a flake-modules-core call

--- flake.nix
+++ flake.nix
@@ -8,11 +8,14 @@
   inputs.flake-compat.flake = false;
   inputs.pre-commit-hooks-nix.url = "github:cachix/pre-commit-hooks.nix";
   inputs.pre-commit-hooks-nix.flake = false;
+  inputs.flake-modules-core.url = "github:hercules-ci/flake-modules-core/template-and-readme";
+  inputs.flake-modules-core.inputs.nixpkgs.follows = "nixos-unstable";
 
   outputs =
     inputs@{ self
     , nixos-20_09
     , nixos-unstable
+    , flake-modules-core
     , ...
     }:
     let
@@ -145,150 +148,154 @@
       };
 
     in
-    {
-      # non-standard attribute
-      ciChecks = lib.mapAttrs (k: v: v // { recurseForDerivations = true; }) allTargets;
+    (flake-modules-core.lib.evalFlakeModule
+      { inherit self; }
+      {
+        flake = {
+          # non-standard attribute
+          ciChecks = lib.mapAttrs (k: v: v // { recurseForDerivations = true; }) allTargets;
 
-      internal.pkgs = lib.mapAttrs (_sys: target: target.internal.pkgs) defaultTarget;
+          internal.pkgs = lib.mapAttrs (_sys: target: target.internal.pkgs) defaultTarget;
 
-      packages =
-        defaultNixpkgs.lib.mapAttrs
-          (
-            system: v:
-              {
-                inherit (v)
-                  hercules-ci-agent
-                  hercules-ci-cli
-                  ;
+          packages =
+            defaultNixpkgs.lib.mapAttrs
+              (
+                system: v:
+                  {
+                    inherit (v)
+                      hercules-ci-agent
+                      hercules-ci-cli
+                      ;
 
-                hercules-ci-agent-nixUnstable =
-                  allTargets."nixos-unstable-nixUnstable".${system}.hercules-ci-agent;
-                hercules-ci-cli-nixUnstable =
-                  allTargets."nixos-unstable-nixUnstable".${system}.hercules-ci-cli;
+                    hercules-ci-agent-nixUnstable =
+                      allTargets."nixos-unstable-nixUnstable".${system}.hercules-ci-agent;
+                    hercules-ci-cli-nixUnstable =
+                      allTargets."nixos-unstable-nixUnstable".${system}.hercules-ci-cli;
 
-                hercules-ci-agent-nix_2_4 = v.hercules-ci-agent;
-                hercules-ci-cli-nix_2_4 = v.hercules-ci-cli;
-              }
-          )
-          defaultTarget;
+                    hercules-ci-agent-nix_2_4 = v.hercules-ci-agent;
+                    hercules-ci-cli-nix_2_4 = v.hercules-ci-cli;
+                  }
+              )
+              defaultTarget;
 
-      overlay =
-        final: prev: (import ./nix/make-overlay.nix inputs) final prev;
+          overlay =
+            final: prev: (import ./nix/make-overlay.nix inputs) final prev;
 
-      # A module like the one in Nixpkgs
-      nixosModules.agent-service =
-        { pkgs, ... }:
-        {
-          _file = "${toString ./flake.nix}#nixosModules.agent-service";
-          imports = [
-            flakeModule
-            ./internal/nix/nixos/default.nix
-          ];
+          # A module like the one in Nixpkgs
+          nixosModules.agent-service =
+            { pkgs, ... }:
+            {
+              _file = "${toString ./flake.nix}#nixosModules.agent-service";
+              imports = [
+                flakeModule
+                ./internal/nix/nixos/default.nix
+              ];
 
-          # This module replaces what's provided by NixOS
-          disabledModules = [ "services/continuous-integration/hercules-ci-agent/default.nix" ];
+              # This module replaces what's provided by NixOS
+              disabledModules = [ "services/continuous-integration/hercules-ci-agent/default.nix" ];
 
-          config = {
-            services.hercules-ci-agent.settings.labels.module = "nixos-service";
+              config = {
+                services.hercules-ci-agent.settings.labels.module = "nixos-service";
+              };
+            };
+
+          # An opinionated module for configuring an agent machine
+          nixosModules.agent-profile =
+            { pkgs, ... }:
+            {
+              _file = "${toString ./flake.nix}#nixosModules.agent-profile";
+              imports = [
+                flakeModule
+                ./internal/nix/nixos/default.nix
+                ./internal/nix/deploy-keys.nix
+                ./internal/nix/gc.nix
+              ];
+
+              # This module replaces what's provided by NixOS
+              disabledModules = [ "services/continuous-integration/hercules-ci-agent/default.nix" ];
+
+              config = {
+                services.hercules-ci-agent.settings.labels.module = "nixos-profile";
+              };
+            };
+
+          # A nix-darwin module
+          darwinModules.agent-service =
+            { pkgs, ... }:
+            {
+              _file = "${toString ./flake.nix}#darwinModules.agent-service";
+              imports = [
+                flakeModule
+                ./internal/nix/nix-darwin/default.nix
+              ];
+
+              # This module replaces what's provided by nix-darwin
+              disabledModules = [ "services/hercules-ci-agent" ];
+
+              config = {
+                services.hercules-ci-agent.settings.labels.module = "darwin-service";
+              };
+            };
+
+          # A nix-darwin module with more defaults set for machines that serve as agents
+          darwinModules.agent-profile =
+            { pkgs, ... }:
+            {
+              _file = "${toString ./flake.nix}#darwinModules.agent-profile";
+              imports = [
+                flakeModule
+                ./internal/nix/nix-darwin/default.nix
+                ./internal/nix/gc.nix
+              ];
+
+              # This module replaces what's provided by nix-darwin
+              disabledModules = [ "services/hercules-ci-agent" ];
+
+              config = {
+                services.hercules-ci-agent.settings.labels.module = "darwin-profile";
+              };
+            };
+
+          defaultApp = lib.mapAttrs (k: v: v.hercules-ci-cli) self.packages;
+
+          defaultTemplate = self.templates.nixos;
+          templates = {
+            nixos = {
+              path = ./templates/nixos;
+              description = "A NixOS configuration with Hercules CI Agent";
+            };
           };
+
+          devShell = lib.mapAttrs
+            (
+              system: { internal, devTools, pre-commit-check, ... }:
+                let
+                  shellWithHaskell = true;
+                  baseShell =
+                    if shellWithHaskell
+                    then import ./nix/shellFor-cabal.nix { inherit internal; }
+                    else internal.pkgs.mkShell { };
+                in
+                baseShell.overrideAttrs (o: {
+                  NIX_PATH = "nixpkgs=${internal.pkgs.path}";
+                  nativeBuildInputs =
+                    o.nativeBuildInputs or [ ] ++ [
+                      devTools.jq
+                      devTools.cabal2nix
+                      devTools.nix-prefetch-git
+                      devTools.valgrind
+                    ] ++ lib.optionals shellWithHaskell [
+                      internal.haskellPackages.haskell-language-server
+                      internal.haskellPackages.implicit-hie # gen-hie
+                      devTools.ghcid
+                    ];
+                  shellHook = ''
+                    ${o.shellHook or ""}
+                    ${pre-commit-check.shellHook}
+                  '';
+                })
+            )
+            defaultTarget;
         };
-
-      # An opinionated module for configuring an agent machine
-      nixosModules.agent-profile =
-        { pkgs, ... }:
-        {
-          _file = "${toString ./flake.nix}#nixosModules.agent-profile";
-          imports = [
-            flakeModule
-            ./internal/nix/nixos/default.nix
-            ./internal/nix/deploy-keys.nix
-            ./internal/nix/gc.nix
-          ];
-
-          # This module replaces what's provided by NixOS
-          disabledModules = [ "services/continuous-integration/hercules-ci-agent/default.nix" ];
-
-          config = {
-            services.hercules-ci-agent.settings.labels.module = "nixos-profile";
-          };
-        };
-
-      # A nix-darwin module
-      darwinModules.agent-service =
-        { pkgs, ... }:
-        {
-          _file = "${toString ./flake.nix}#darwinModules.agent-service";
-          imports = [
-            flakeModule
-            ./internal/nix/nix-darwin/default.nix
-          ];
-
-          # This module replaces what's provided by nix-darwin
-          disabledModules = [ "services/hercules-ci-agent" ];
-
-          config = {
-            services.hercules-ci-agent.settings.labels.module = "darwin-service";
-          };
-        };
-
-      # A nix-darwin module with more defaults set for machines that serve as agents
-      darwinModules.agent-profile =
-        { pkgs, ... }:
-        {
-          _file = "${toString ./flake.nix}#darwinModules.agent-profile";
-          imports = [
-            flakeModule
-            ./internal/nix/nix-darwin/default.nix
-            ./internal/nix/gc.nix
-          ];
-
-          # This module replaces what's provided by nix-darwin
-          disabledModules = [ "services/hercules-ci-agent" ];
-
-          config = {
-            services.hercules-ci-agent.settings.labels.module = "darwin-profile";
-          };
-        };
-
-      defaultApp = lib.mapAttrs (k: v: v.hercules-ci-cli) self.packages;
-
-      defaultTemplate = self.templates.nixos;
-      templates = {
-        nixos = {
-          path = ./templates/nixos;
-          description = "A NixOS configuration with Hercules CI Agent";
-        };
-      };
-
-      devShell = lib.mapAttrs
-        (
-          system: { internal, devTools, pre-commit-check, ... }:
-            let
-              shellWithHaskell = true;
-              baseShell =
-                if shellWithHaskell
-                then import ./nix/shellFor-cabal.nix { inherit internal; }
-                else internal.pkgs.mkShell { };
-            in
-            baseShell.overrideAttrs (o: {
-              NIX_PATH = "nixpkgs=${internal.pkgs.path}";
-              nativeBuildInputs =
-                o.nativeBuildInputs or [ ] ++ [
-                  devTools.jq
-                  devTools.cabal2nix
-                  devTools.nix-prefetch-git
-                  devTools.valgrind
-                ] ++ lib.optionals shellWithHaskell [
-                  internal.haskellPackages.haskell-language-server
-                  internal.haskellPackages.implicit-hie # gen-hie
-                  devTools.ghcid
-                ];
-              shellHook = ''
-                ${o.shellHook or ""}
-                ${pre-commit-check.shellHook}
-              '';
-            })
-        )
-        defaultTarget;
-    };
+      }).config.flake;
 }
