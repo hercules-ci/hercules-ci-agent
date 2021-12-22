61d80c8caf6fb62bb2052bb1bc49015860dcdde4
Robert Hensing <robert@roberthensing.nl>
2021-12-22 01:53:22 +0100

    flake.nix: Remove redundant nixos-20_09

--- flake.nix
+++ flake.nix
@@ -1,7 +1,6 @@
 {
   description = "Hercules CI Agent";
 
-  inputs.nixos-20_09.url = "github:NixOS/nixpkgs/nixos-20.09";
   inputs.nixos-unstable.url = "github:NixOS/nixpkgs/master";
   inputs.nix-darwin.url = "github:LnL7/nix-darwin"; # test only
   inputs.flake-compat.url = "github:edolstra/flake-compat";
@@ -12,7 +11,6 @@
 
   outputs =
     inputs@{ self
-    , nixos-20_09
     , nixos-unstable
     , flake-modules-core
     , ...
