0589ff6a82530207549700dbca1b036622e1689b
Robert Hensing <robert@roberthensing.nl>
2022-03-07 20:50:08 +0100

    WIP update

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
   description = "Hercules CI Agent";
 
-  inputs.nixos-unstable.url = "github:NixOS/nixpkgs/haskell-updates";
+  inputs.nixos-unstable.url = "github:hercules-ci/nixpkgs/haskell-updates";
   inputs.nix-darwin.url = "github:LnL7/nix-darwin"; # test only
   inputs.flake-compat.url = "github:edolstra/flake-compat";
   inputs.flake-compat.flake = false;
