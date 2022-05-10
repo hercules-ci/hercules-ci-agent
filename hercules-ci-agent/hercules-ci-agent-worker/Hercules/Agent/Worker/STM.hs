1a8c3f2be5049cc35705bc4ef64c10c06138d1c7
Robert Hensing <robert@roberthensing.nl>
2022-05-10 16:38:14 +0200

    Add Hercules.Agent.Worker.STM module

---
+++ hercules-ci-agent/hercules-ci-agent-worker/Hercules/Agent/Worker/STM.hs
@@ -0,0 +1,48 @@
+{-# LANGUAGE BlockArguments #-}
+
+module Hercules.Agent.Worker.STM where
+
+import Control.Concurrent.Async
+import Control.Concurrent.STM (TVar, readTVar, writeTVar)
+import Control.Monad.Fix (mfix)
+import qualified Data.Map as M
+import Protolude
+
+asyncIfSTM ::
+  -- | Either return an existing async or return @b@ as an input to a new one.
+  --
+  -- The returned transaction must be not evaluate the 'Async' argument,
+  -- but can write it somewhere.
+  (Async a -> STM (Either (Async a) b)) ->
+  -- | The action to run when the STM transaction decided to create a new 'Async'.
+  (b -> IO a) ->
+  -- | Either the async returned by the STM transaction, or a new async waiting
+  -- for the provided IO.
+  IO (Async a)
+asyncIfSTM cond io = mfix \lazyAsync ->
+  atomically (cond lazyAsync) >>= \case
+    Left early -> pure early
+    Right b -> do
+      async (io b)
+
+ensureTVarMapItem :: Ord k => k -> STM v -> TVar (Map k v) -> STM (Bool, v)
+ensureTVarMapItem key mkValue mapVar = do
+  map0 <- readTVar mapVar
+  case M.lookup key map0 of
+    Nothing -> do
+      value <- mkValue
+      writeTVar mapVar (M.insert key value map0)
+      pure (False, value)
+    Just value0 -> do
+      pure (True, value0)
+
+asyncInTVarMap :: Ord k => k -> TVar (Map k (Async a)) -> IO a -> IO (Async a)
+asyncInTVarMap key mapVar action =
+  asyncIfSTM
+    ( \asy -> do
+        (existed, v) <- ensureTVarMapItem key (pure asy) mapVar
+        if existed
+          then pure (Left v)
+          else pure (Right ())
+    )
+    (\() -> action)
--- hercules-ci-agent/test/Spec.hs
+++ hercules-ci-agent/test/Spec.hs
@@ -5,6 +5,7 @@ where
 
 import qualified Hercules.Agent.Nix.RetrieveDerivationInfoSpec
 import qualified Hercules.Agent.NixPathSpec
+import qualified Hercules.Agent.Worker.STMSpec
 import qualified Hercules.Agent.WorkerProcessSpec
 import qualified Hercules.SecretsSpec
 import Test.Hspec
@@ -15,3 +16,4 @@ spec = do
   describe "Hercules.Agent.WorkerProcessSpec" Hercules.Agent.WorkerProcessSpec.spec
   describe "Hercules.Agent.Nix.RetrieveDerivationInfo" Hercules.Agent.Nix.RetrieveDerivationInfoSpec.spec
   describe "Hercules.Secret" Hercules.SecretsSpec.spec
+  describe "Hercules.Agent.Worker.STMSpec" Hercules.Agent.Worker.STMSpec.spec
