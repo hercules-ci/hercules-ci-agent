df4cc886c56affd76bdd02bab54c0ac803cdab3b
Robert Hensing <robert@roberthensing.nl>
2022-11-23 18:40:51 +0000

    hercules-ci-agent: Improve narinfo-cache-negative-ttl message

--- hercules-ci-agent/hercules-ci-agent/Hercules/Agent/Nix/Init.hs
+++ hercules-ci-agent/hercules-ci-agent/Hercules/Agent/Nix/Init.hs
@@ -11,19 +11,22 @@ newEnv = do
   when (EnvironmentInfo.nixNarinfoCacheNegativeTTL nixInfo /= 0) $ do
     putErrText
       "\n\
-      \We have detected that the setting narinfo-cache-negative-ttl is non-zero.\n\
+      \We have detected that the Nix setting narinfo-cache-negative-ttl is non-zero.\n\
       \Running hercules-ci-agent on a system with a non-zero negative ttl will cause\n\
       \problems when run in a cluster.\n\
       \Note that this setting only affects the caching of paths that are *missing*\n\
       \from a cache. Paths that *are* in the binary cache are cached as configured in\n\
-      \the 'positive' option.\n\
+      \the narinfo-cache-positive-ttl option. You don't need to configure the positive\n\
+      \option. \n\
       \\n\
       \On NixOS and nix-darwin, use the recommended installation method via module,\n\
       \make sure that the `narinfo-cache-negative-ttl` isn't set via other means.\n\
       \If you can't use the module, use\
+      \    nix.settings.narinfo-cache-negative-ttl = 0;\n\
+      \  or\n\
       \    nix.extraOptions = \"narinfo-cache-negative-ttl = 0\"\n\
       \\n\
-      \or add to your system nix.conf:\n\
+      \or add to your system /etc/nix/nix.conf:\n\
       \    narinfo-cache-negative-ttl = 0\n\
       \n"
     throwIO $ FatalError "Please configure your system's Nix with:  narinfo-cache-negative-ttl = 0  "
