fd28b824216b5a8c530e63d080ca1f0798e49968
Robert Hensing <robert@roberthensing.nl>
2023-06-27 20:08:37 +0200

    flake: Add a check to build against latest cachix
    Currently requires keeping the commit hash in sync with the submodule...
    Needs a better flake handler in hci itself.

---
+++ nix/flake-cachix-dev.nix
@@ -0,0 +1,24 @@
+{ lib, config, ... }:
+{
+  # FIXME: this is incredibly ad hoc; use an extra hercules ci handler with submodules enabled instead (to be implemented)
+  #        (alternatively, do something with pre-commit to sync the submodule hash)
+  variants.cachix-dev = {
+    perSystem = { system, pkgs, ... }: {
+      haskellProjects.internal = {
+        overrides = self: super:
+          let
+            flake = builtins.getFlake "git+https://github.com/cachix/cachix.git?rev=498f2fa43de8b2fc2e372c275dece0715101cbda&allRefs=true";
+            c = flake.lib.customHaskellPackages { inherit pkgs; haskellPackages = self; };
+          in
+          # If the overrides change, we need to make sure it's not hercules-ci-cnix-store for example
+          assert lib.attrNames c == [ "cachix" "cachix-api" ];
+          c;
+      };
+    };
+  };
+  perSystem = { system, ... }:
+    # Only build with dev version of cachix on x86_64-linux, as it's mostly about the conditional compilation anyway, and we don't distribute this version.
+    lib.optionalAttrs (system == "x86_64-linux") {
+      checks.build-with-cachix-1_6 = config.variants.cachix-dev.allSystems.${system}.packages.hercules-ci-agent;
+    };
+}
