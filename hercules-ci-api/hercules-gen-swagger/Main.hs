c804dc7d5021e251c3096d14faba2410bec0818b
Robert Hensing <robert@roberthensing.nl>
2024-01-07 21:23:36 +0100

    hercules-gen-swagger: Add --openapi3 flag

--- hercules-ci-api/hercules-gen-swagger/Main.hs
+++ hercules-ci-api/hercules-gen-swagger/Main.hs
@@ -5,8 +5,14 @@ where
 
 import Data.Aeson (encode)
 import Data.String.Conv (toS)
-import Hercules.API (swagger)
+import Hercules.API (openapi3, swagger)
+import System.Environment (getArgs)
 import Prelude
 
 main :: IO ()
-main = putStrLn $ toS $ encode swagger
+main = do
+  args <- getArgs
+  case args of
+    ["--openapi3"] -> putStrLn $ toS $ encode openapi3
+    [] -> putStrLn $ toS $ encode swagger
+    _ -> error $ "Unknown arguments" <> show args
