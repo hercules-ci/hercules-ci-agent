8420a5b1128558fb80e4cfbdcf4dfa29c77e61fa
Robert Hensing <robert@roberthensing.nl>
2024-05-03 00:54:45 +0200

    fix: Replace bad generated OpenAPI3 identifier

--- hercules-ci-api/hercules-gen-swagger/Main.hs
+++ hercules-ci-api/hercules-gen-swagger/Main.hs
@@ -4,7 +4,9 @@ module Main
 where
 
 import Data.Aeson (encode)
+import Data.Function ((&))
 import Data.String.Conv (toS)
+import Data.Text qualified as T
 import Hercules.API (openapi3, swagger)
 import System.Environment (getArgs)
 import System.IO (hPutStrLn, stderr)
@@ -14,7 +16,13 @@ main :: IO ()
 main = do
   args <- getArgs
   case args of
-    ["--experimental-openapi3"] -> putStrLn $ toS $ encode openapi3
+    ["--experimental-openapi3"] ->
+      putStrLn $
+        openapi3
+          & encode
+          & toS
+          & T.replace "Attribute_(Result_AttributeError_Derivation)" "Attribute_Result_AttributeError_Derivation"
+          & toS
     [] -> putStrLn $ toS $ encode swagger
     _ -> do
       hPutStrLn stderr "Usage: hercules-gen-swagger [--experimental-openapi3] > swagger.json"
