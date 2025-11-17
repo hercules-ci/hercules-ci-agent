3b411f08c5270ff8db2a5f721a5d3e81ef1a326c
Robert Hensing <robert@roberthensing.nl>
2025-11-17 09:50:01 +0100

    fix: Avoid bcopy deprecation
        error: 'bcopy' is deprecated: bcopy is deprecated. Use memcpy or memmove instead [-Werror,-Wdeprecated-declarations]

--- hercules-ci-cnix-store/cbits/string.cxx
+++ hercules-ci-cnix-store/cbits/string.cxx
@@ -1,10 +1,11 @@
 #include "hercules-ci-cnix/string.hxx"
+#include <cstring>
 
 namespace hercules_ci_cnix {
 
 char * stringdup(const std::string & s) {
     char * p = (char *)malloc(s.size() + 1);
-    std::copy(s.begin(), s.end(), p);
+    std::memcpy(p, s.data(), s.size());
     p[s.size()] = '\0';
     return p;
 }
