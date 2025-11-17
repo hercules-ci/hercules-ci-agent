0f21bd02836c95004adeb33b1b99cc32a09919a6
Robert Hensing <robert@roberthensing.nl>
2025-11-17 09:50:29 +0100

    maint: Improve DerivationInputsIterator definition
    It is equivalent, but this more closely matches how Nix itself
    uses this type.

--- hercules-ci-cnix-store/include/hercules-ci-cnix/store.hxx
+++ hercules-ci-cnix-store/include/hercules-ci-cnix/store.hxx
@@ -2,6 +2,9 @@
 #pragma once
 
 #include <nix/store/path-info.hh>
+#if NIX_IS_AT_LEAST(2, 29, 0)
+#include <nix/store/derived-path-map.hh>
+#endif
 
 typedef nix::ref<nix::Store> refStore;
 
@@ -11,8 +14,9 @@ typedef nix::StringPairs::iterator StringPairsIterator;
 typedef nix::PathSet::iterator PathSetIterator;
 typedef nix::ref<const nix::ValidPathInfo> refValidPathInfo;
 
+// https://github.com/NixOS/nix/pull/13129
 #if NIX_IS_AT_LEAST(2, 29, 0)
-typedef nix::DerivedPathMap<std::set<nix::OutputName, std::less<>>>::Map::iterator DerivationInputsIterator;
+typedef nix::DerivedPathMap<nix::StringSet>::Map::iterator DerivationInputsIterator;
 #else
 typedef nix::DerivedPathMap<std::set<nix::OutputName>>::Map::iterator DerivationInputsIterator;
 #endif
